7e3e8461c8d7a45afa9bbf25b58fca64f9df7dad
ThiDev103 <125152726+ThiDev103@users.noreply.github.com>
2023-02-25 15:48:55 -0300

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,43 @@
+##import this
+
+##print('ola mundo')
+
+num1 = 10
+num2 = 5
+
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+exponeciacao = num1 ** num2
+restoD = num1 % num2
+
+
+print("A soma é: ", soma, "A subtração é: " , subtracao, "A multiplicação: ",multiplicacao , "A divisão é: ", divisao, "a exponenciação:", exponeciacao, "resto da divisão:", restoD, ) 
+
+resultado = (10+3)*7/2-3 **2
+print("Resultado é: ", resultado)
+
+nume1 = 10 
+nume2 = 20 
+nume3 = 30 
+
+media = (nume1 + nume2 + nume3)/3
+print("A média é: ", media)
+
+
+idade = 18
+ano = 2023
+nome = "Fernandinho"
+altura = 1.75
+print(type(idade))
+print(type(ano))
+print(type(nome)(type(altura)))
+
+
+numero = int(input("digite um numero: "))
+print(numero)
+numero1 = numero +1
+numero2 = numero -1
+print("o numero é:",numero,"Seu Sucessor é:", numero1,"Seu antecessor é:", numero2)
